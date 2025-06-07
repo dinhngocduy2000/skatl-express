3f29adaba2d01d67ff31276e8b9757f7e560f156
Dylan Dinh <ngocduydinh2000@gmail.com>
2025-06-07 23:06:29 +0700

    feat: setting up router

---
+++ dist/router/index.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const router = Router();
+// Define your routes here
+router.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+export default router;
+//# sourceMappingURL=index.js.map
---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import express from "express";
+import router from "./router";
+import testRouter from "./router/v1/test";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const APP_PREFIX = "/api/v1";
+
+// const AppDataSource = new DataSource({
+//   type: "postgres",
+//   host: "localhost",
+//   port: 5432,
+//   username: "your_username",
+//   password: "your_password",
+//   database: "your_database",
+//   synchronize: false,
+//   logging: false,
+//   entities: ["src/entity/**/*.ts"],
+//   migrations: ["src/migration/**/*.ts"],
+//   subscribers: ["src/subscriber/**/*.ts"],
+// });
+
+// AppDataSource.initialize()
+//   .then(async () => {
+//     app.use("/api/v1", router);
+//     app.use(`${APP_PREFIX}/test`, testRouter);
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => console.log("Data Source initialization error: ", error));
+
+app.use("/api/v1", router);
+app.use(`${APP_PREFIX}/test`, testRouter);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
