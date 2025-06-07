c7d26bb59254b0a9ffeda9cbdaf60f00019f142d
Dylan Dinh <ngocduydinh2000@gmail.com>
2025-06-07 23:33:11 +0700

    feat: adding env

--- src/app.ts
+++ src/app.ts
@@ -4,39 +4,48 @@ import express from "express";
 import router from "./router";
 import testRouter from "./router/v1/test";
 import cors from "cors";
+import dotenv from "dotenv";
 
+// Load environment variables from .env file
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.APP_PORT || 3000;
 const APP_PREFIX = "/api/v1";
 app.use(
   cors({
     origin: "*",
   })
 );
-const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "your_username",
-  password: "your_password",
-  database: "your_database",
-  synchronize: false,
-  logging: false,
-  entities: ["src/entity/**/*.ts"],
-  migrations: ["src/migration/**/*.ts"],
-  subscribers: ["src/subscriber/**/*.ts"],
-});
+// const AppDataSource = new DataSource({
+//   type: "postgres",
+//   host: "localhost",
+//   port: 5432,
+//   username: "your_username",
+//   password: "your_password",
+//   database: "your_database",
+//   synchronize: false,
+//   logging: false,
+//   entities: ["src/entity/**/*.ts"],
+//   migrations: ["src/migration/**/*.ts"],
+//   subscribers: ["src/subscriber/**/*.ts"],
+// });
 
-AppDataSource.initialize()
-  .then(async () => {
-    app.use((req, res, next) => {
-      console.log("Time:", Date.now());
-      next();
-    });
-    app.use("/api/v1", router);
-    app.use(`${APP_PREFIX}/test`, testRouter);
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((error) => console.log("Data Source initialization error: ", error));
+// AppDataSource.initialize()
+//   .then(async () => {
+//     app.use((req, res, next) => {
+//       console.log("Time:", Date.now());
+//       next();
+//     });
+//     app.use("/api/v1", router);
+//     app.use(`${APP_PREFIX}/test`, testRouter);
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => console.log("Data Source initialization error: ", error));
+
+app.use("/api/v1", router);
+app.use(`${APP_PREFIX}/test`, testRouter);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
